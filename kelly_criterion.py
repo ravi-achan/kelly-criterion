b13512cb4bb225171b8096fba31e05cbc9363b03
raven250 <67653646+raven250@users.noreply.github.com>
2021-12-03 20:19:39 +0000

    changed old code to functional form

--- kelly_criterion.py
+++ kelly_criterion.py
@@ -1,75 +1,57 @@
 #Interactive Tool Visualising Kelly's Criterion
-
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-print('What are the odds in this scenario? (Decimal Odds): ')
-o = input()
-odds = float(o)
-
-print('What is the probability in this scenario? (as %): ')
-p = input()
-probability = int(p)
-q = 100 - probability
-
-bankroll_initial = 100.0
-
-print('Stake proportion of bankroll (as %): ')
-s = input()
-stake = float(s)/100
-
-
-#Initialising axes
-x = np.arange(0, 1000, 1)
-y = np.zeros(1000)
-
-bankroll = bankroll_initial
-y[0] = bankroll_initial
-
-#Calculating user bankroll array
-for i in range(1, 1000):
-    outcomes = ['Win'] * probability + ['Lose'] * q
-    result = random.choice(outcomes)
-
-    if(result == 'Win'):
-        bankroll = bankroll - (stake*bankroll) + (stake*bankroll*odds)
-        y[i] = bankroll
-
-    elif(result == 'Lose'):
-        bankroll = bankroll - (bankroll*stake)
-        y[i] = bankroll
-
-#Calculating Kelly's bankroll array
-k = np.zeros(1000)
-k[0] = bankroll_initial
-k_bankroll = bankroll_initial
-kelly_stake = ((probability/100)*(odds-1) - (q/100))/(odds-1)
-if(kelly_stake > 0):
-
-    for i in range(1, 1000):
-        outcomes = ['Win'] * probability + ['Lose'] * q
+def outcome_user_stake(bank_init, stake, odds, probability, trials):
+    y = np.zeros(trials)
+    bankroll = bank_init
+    y[0] = bank_init
+    outcomes = ['Win'] * int(probability*100) + ['Lose']*int((1-probability)*100)
+    for i in range(1,trials):
         result = random.choice(outcomes)
 
         if(result == 'Win'):
-            k_bankroll = k_bankroll - (kelly_stake*k_bankroll) + (kelly_stake*k_bankroll*odds)
-            k[i] = k_bankroll
+            bankroll = bankroll + (stake*bankroll*odds) - (stake*bankroll)
+            y[i] = bankroll
 
         elif(result == 'Lose'):
-            k_bankroll = k_bankroll - (k_bankroll*kelly_stake)
-            k[i] = k_bankroll
+            bankroll = bankroll - (stake*bankroll)
+            y[i] = bankroll
+    return y
 
-#If Kelly's function is negative the optimal outcome is to not stake anything
-else:
-    for i in range(1, 1000):
-        k[i] = k_bankroll
+def outcome_kelly_stake(bank_init, odds, probability, trials):
 
+    kelly_stake = probability - ((1-probability)/(odds-1))
+    if kelly_stake > 0:
+        return outcome_user_stake(bank_init, kelly_stake, odds, probability, trials)
+    else:
+        y = np.zeros(trials)
+        y.fill(bank_init)
+        return y
 
+def averaged_outcome_n_repeats(n, bank_init, stake, odds, probability, trials):
+    y = np.zeros(trials)
+    k = np.zeros(trials)
 
-#Graphing
-plt.plot(x,y, color="#228B22")
-plt.plot(x,k, color="#800080")
-plt.title('Kelly Criterion')
-plt.xlabel('Number of Trials')
-plt.ylabel('Bankroll')
-plt.show()
+    for i in range(n):
+        y += outcome_user_stake(bank_init, stake, odds, probability, trials)
+        k += outcome_kelly_stake(bank_init, odds, probability, trials)
+
+    return y/n, k/n
+
+def plotting(n, bank_init, stake, odds, probability, trials):
+    x = np.arange(0,trials,1)
+    y, k = averaged_outcome_n_repeats(n, bank_init, stake, odds, probability, trials)
+
+    plt.plot(x,y, color="#228B22") #user plots in green
+    plt.plot(x,k, color="#800080") #kelly plots in purple
+    plt.title('Kelly Criterion')
+    plt.xlabel('Number of Trials')
+    plt.ylabel('Bankroll')
+    plt.show()
+
+print(outcome_user_stake(100, 0.20, 1.58, 0.7, 100))
+print(outcome_kelly_stake(100, 1.58, 0.7, 100))
+
+plotting(1000, 100, 0.20, 1.58, 0.7, 100)
